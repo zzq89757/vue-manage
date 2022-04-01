c0423f0fe773180d7406377ee9104c4f5d2fac4c
zzq89757 <1604653835@qq.com>
2022-04-02 03:53:38 +0800

    发送更改密码请求

--- src/views/others/Profile.vue
+++ src/views/others/Profile.vue
@@ -1,9 +1,13 @@
 <!--  -->
 <template>
-  <div></div>
+  <div>
+    <el-button @click="updatePwd()">更改密码</el-button>
+  </div>
 </template>
 
 <script>
+import axios from '@/api/axios';
+import QueryString from 'qs';
 export default {
   data () {
     return {
@@ -16,7 +20,14 @@ export default {
 
   mounted: {},
 
-  methods: {}
+  methods: {
+    updatePwd(){
+      axios.request({method:'post',url:'/api/updatepwd',data:QueryString.stringify({
+        oldPwd:'111111',
+        newPwd:'111111a',
+      })}).then(res=>console.log(res)).catch(err=>console.log(err))
+    }
+  }
 }
 
 </script>
